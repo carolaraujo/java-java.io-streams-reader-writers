2cd7b66f22b8983df3aa9da845f6cd78d659110b
Caroline Araujo <ca.araujosouza@gmail.com>
2021-03-30 22:18:00 -0300

    entradas e saidas de arquivos com classes abstratas do java.io

---
+++ java-io/src/br/com/alura/java/io/teste/TesteEscrita.java
@@ -0,0 +1,29 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class TesteEscrita {
+	
+	public static void main(String[] args) throws IOException{
+		
+		//Fluxo de Saída de Arquivo
+		OutputStream fos = new FileOutputStream("lorem.txt");
+		Writer osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		bw.write("hjbahisdbuiabduiadbuiasbdiawudfbaw");
+		bw.newLine();
+		bw.newLine();
+		bw.newLine();
+		bw.write("akjfbnaijfbaufbauisfbuioasfbweuiofb");
+		
+		
+		bw.close();
+	}
+
+}
